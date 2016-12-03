742eff9b2b2b777450f88bab108c7acaae2eb6c0
rhamill1 <rhamill11@gmail.com>
2016-12-03 12:10:16 -0800

    sets all directories under ruby

---
+++ ruby/spec/ruby_array_addition_spec.rb
@@ -0,0 +1,12 @@
+# require 'spec_helper'
+require './scripts/ruby_array_addition.rb'
+
+RSpec.describe Array, 'end_arr_add' do
+  context 'Add element to the end of the Array variable `arr`' do
+    it 'and return `arr`' do
+      arr = ['a','b','c','d','e','f']
+      element = 'the element'
+      expect(end_arr_add(arr, element)).to eq ['a','b','c','d','e','f','the element']
+    end
+  end
+end

a9e2c64d5f275c1a853d44dc6b283af54325ecf9
rhamill1 <rhamill11@gmail.com>
2016-12-05 10:27:20 -0800

    adds begin_arr_add

--- ruby/scripts/ruby_array_addition.rb
+++ ruby/scripts/ruby_array_addition.rb
@@ -2,3 +2,7 @@
 def end_arr_add(arr, element)
   arr.push(element)
 end
+
+def begin_arr_add(arr, element)
+  arr.insert(0, element)
+end
--- ruby/spec/ruby_array_addition_spec.rb
+++ ruby/spec/ruby_array_addition_spec.rb
@@ -1,12 +1,23 @@
 # require 'spec_helper'
 require './scripts/ruby_array_addition.rb'
 
+
 RSpec.describe Array, 'end_arr_add' do
   context 'Add element to the end of the Array variable `arr`' do
     it 'and return `arr`' do
       arr = ['a','b','c','d','e','f']
       element = 'the element'
       expect(end_arr_add(arr, element)).to eq ['a','b','c','d','e','f','the element']
+    end
+  end
+end
+
+RSpec.describe Array, 'begin_arr_add' do
+  context 'Add `element` to the beginning of the Array variable `arr` and return `arr`' do
+    it 'and return `arr`' do
+      arr = ['a','b','c','d','e','f']
+      element = 'the element'
+      expect(begin_arr_add(arr, element)).to eq ['the element','a','b','c','d','e','f']
     end
   end
 end

1dacb86c624418381ddac0d578a7b4980ea7617b
rhamill1 <rhamill11@gmail.com>
2016-12-05 17:05:16 -0800

    removes dead code from delete_spec

--- ruby/spec/ruby_array_deletion_spec.rb
+++ ruby/spec/ruby_array_deletion_spec.rb
@@ -2,7 +2,7 @@ require './scripts/ruby_array_deletion.rb'
 
 arr = [5, 6, 5, 4, 3, 1, 2, 5, 4, 3, 3, 3]
 
-RSpec.describe Array, 'end_arr_delete' do
+describe 'end_arr_delete' do
   context 'delete the element from the end of the array and return the deleted element' do
     it 'deletes the last element in an array' do
       expect(end_arr_delete(arr)).to eq 3
@@ -10,7 +10,7 @@ RSpec.describe Array, 'end_arr_delete' do
   end
 end
 
-RSpec.describe Array, 'start_arr_delete' do
+describe 'start_arr_delete' do
   context 'delete the element at the beginning of the array and return the deleted element' do
     it 'deletes the first element in an array' do
       expect(start_arr_delete(arr)).to eq 5
@@ -18,7 +18,7 @@ RSpec.describe Array, 'start_arr_delete' do
   end
 end
 
-RSpec.describe Array, 'delete_at_arr' do
+describe 'delete_at_arr' do
   context 'delete the element at the position index' do
     it 'deletes the element at the index of an array' do
       expect(delete_at_arr(arr, 2) {arr[2]}).to eq 4
@@ -26,7 +26,7 @@ RSpec.describe Array, 'delete_at_arr' do
   end
 end
 
-RSpec.describe Array, 'delete_all' do
+describe 'delete_all' do
   context 'delete all the elements of the array where element = val' do
     it 'deletes all occurances of a value in an array' do
       delete_all(arr, 5)

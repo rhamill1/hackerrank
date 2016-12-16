a0e2f826e38aed882324252e229baeb3732058b4
rhamill1 <rhamill11@gmail.com>
2016-12-15 17:48:18 -0800

    adds default hash init

--- ruby/scripts/ruby_hash_initialize.rb
+++ ruby/scripts/ruby_hash_initialize.rb
@@ -1,3 +1,7 @@
 def init_empty_hash
   $empty_hash = Hash.new
 end
+
+def init_default_hash
+  $default_hash = Hash.new(1)
+end
--- ruby/spec/ruby_hash_initialize_spec.rb
+++ ruby/spec/ruby_hash_initialize_spec.rb
@@ -11,8 +11,32 @@ describe 'init_empty_hash' do
 end
 
 describe 'init_empty_hash' do
-  it 'is of type hash' do
+  it 'is of a hash' do
     init_empty_hash()
     expect($empty_hash).to eq({})
+  end
+end
+
+describe 'init_default_hash' do
+  context 'initializes an empty Hash with the variable name default_hash and the default value of every key set to 1' do
+    it 'is empty' do
+      init_default_hash()
+      expect($default_hash).to be_empty
+    end
+  end
+end
+
+describe 'init_default_hash' do
+  it 'is of a hash' do
+    init_empty_hash()
+    expect($default_hash).to eq({})
+  end
+end
+
+describe 'init_default_hash' do
+  it 'has a default value of 1' do
+    init_empty_hash()
+    $default_hash[:key] = "bar"
+    expect($default_hash[:bar]).to eq 1
   end
 end

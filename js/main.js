131b8742855e598b6b9c07686c7ef36e028fdc77
rhamill1 <rhamill11@gmail.com>
2017-01-30 19:47:36 -0800

    sum of multipes function passes 2 tests

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 exports.multiplesSum = function(maxNumber) {
-  return 'your test fails';
+
+  var maxNumber = maxNumber;
+  var allNumbers = 0;
+
+  for (var i = maxNumber - 1; i > 0; i --){
+    if (i % 3 == 0 || i % 5 == 0){
+      allNumbers += i;
+    };
+  };
+
+  return allNumbers;
+
 };
--- js/test/test.js
+++ js/test/test.js
@@ -1,10 +1,16 @@
-var main = require("../main.js");
-
+var main = require('../main.js');
 var assert = require('assert');
-describe('multiplesSum()', function() {
-  describe('multiplesSum(1000)', function() {
-    it('If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. Find the sum of all the multiples of 3 or 5 below 1000', function() {
-      assert.equal(2000, main.multiplesSum(1000));
-    });
+
+
+describe('multiplesSum(1000)', function() {
+  it('Find the sum of all the multiples of 3 or 5 below 1000', function() {
+    assert.equal(233168, main.multiplesSum(1000));
+  });
+});
+
+
+describe('multiplesSum(10)', function() {
+  it('Find the sum of all the multiples of 3 or 5 below 1000', function() {
+    assert.equal(23, main.multiplesSum(10));
   });
 });

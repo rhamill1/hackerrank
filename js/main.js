e2bdf04a207a15ed0d9b9c06bf3b4a7ecd423e1a
rhamill1 <rhamill11@gmail.com>
2017-01-31 16:55:49 -0800

    adds thirdLargestNumber and reverseString to JS

--- js/main.js
+++ js/main.js
@@ -12,3 +12,41 @@ exports.multiplesSum = function(maxNumber) {
   return allNumbers;
 
 };
+
+
+exports.thirdLargestNumber =  function(array) {
+  if (array.length < 3) {
+    return 'array too short';
+  }
+  else {
+    var sortedArray = array.sort(function(a, b){return b - a});
+    var largestNumbersArray = [];
+
+    for (var i = 0; sortedArray.length > i && largestNumbersArray.length < 3 ; i++){
+      var nextIndex = i + 1;
+      if (sortedArray[i] != sortedArray[nextIndex]) {
+        largestNumbersArray.push(sortedArray[i]);
+      };
+    };
+
+    if (largestNumbersArray.length < 3) {
+      return 'not enough unique values in array';
+    }
+    else {
+      return largestNumbersArray[2];
+    }
+
+  };
+};
+
+
+exports.stringReverse = function(string){
+
+  var newString = '';
+  for (var i = string.length; string.length > 0; i--){
+    newString += string.charAt(string.length - 1);
+    string = string.slice(0, -1);
+  }
+
+  return newString;
+}
--- js/test/test.js
+++ js/test/test.js
@@ -8,9 +8,44 @@ describe('multiplesSum(1000)', function() {
   });
 });
 
-
 describe('multiplesSum(10)', function() {
   it('Find the sum of all the multiples of 3 or 5 below 1000', function() {
     assert.equal(23, main.multiplesSum(10));
+  });
+});
+
+
+
+
+describe('stringReverse("pizza")', function() {
+  it('returns one input string reversed', function() {
+    assert.equal('azzip', main.stringReverse('pizza'));
+  });
+});
+
+
+
+
+describe('thirdLargestNumber([1,2,7,8,9,3,5,0,2,7,8])', function() {
+  it('returns the 3rd largest unique number', function() {
+    assert.equal(7, main.thirdLargestNumber([1,2,7,8,9,3,5,0,2,7,8]));
+  });
+});
+
+describe('thirdLargestNumber([3,8,8,8,1,10,10])', function() {
+  it('returns the 3rd largest unique number', function() {
+    assert.equal(3, main.thirdLargestNumber([3,8,8,8,1,10,10]));
+  });
+});
+
+describe('thirdLargestNumber([1,2,2])', function() {
+  it('returns the 3rd largest unique number', function() {
+    assert.equal('not enough unique values in array', main.thirdLargestNumber([1,2,2]));
+  });
+});
+
+describe('thirdLargestNumber([1,2])', function() {
+  it('returns the 3rd largest unique number', function() {
+    assert.equal('array too short', main.thirdLargestNumber([1,2]));
   });
 });
